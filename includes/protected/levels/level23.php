581277330cf4818e988e919cf00998dd022253ea
Gaurvendra <44363966+Gaurvendra@users.noreply.github.com>
2020-04-19 15:39:31 +0530

    Update level23.php

--- includes/protected/levels/level23.php
+++ includes/protected/levels/level23.php
@@ -41,7 +41,7 @@ $comment = "<!-- 71 142 107 205 -->";
                       <div class="panel-body">
                             <p>
 
-<img src="<?php echo $img ?>" width="300px" height="300px">
+<img src="<?php echo $img; ?>" width="300px" height="300px">
 <p> </div>
     <div class="panel-footer"></div>
 </div>
