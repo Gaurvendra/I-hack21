50e3ec501b5180e28c4c86fffc59eabbb94c149c
Gaurvendra <44363966+Gaurvendra@users.noreply.github.com>
2020-04-19 15:03:56 +0530

    Update level22.php

--- includes/protected/levels/level22.php
+++ includes/protected/levels/level22.php
@@ -38,9 +38,11 @@ $comment = "<!-- 109 10 111 104 135 55 261 -->";
 <div class="panel panel-c-white">
     <div class="panel-heading">
     </div>
-    <div class="panel-body">
-        <img src = "<?php echo $img; ?>"></img>
-     </div>
+                       <div class="panel-body">
+                            <p>
+
+<img src="<?php echo $img ?>" width="300px" height="300px">
+<p> </div>
     <div class="panel-footer"></div>
 </div>
 
