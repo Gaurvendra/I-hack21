54c6e58281d1564a30d021180f904dc2ecf2f0e4
Ishtpreet Singh <500061526@stu.upes.ac.in>
2020-05-02 22:51:40 +0530

    phonenumber

--- register.php
+++ register.php
@@ -127,7 +127,8 @@ if (name==null || name==""){
   alert("Please Enter A Valid Contact no. ");   
   return false;
    
-}else if(num1.length!=10){
+}
+if(num1.length!=10){
    alert("Please enter a valid contact no. ");  
   return false;
 }
