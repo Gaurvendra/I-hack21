3359ff56bae30d51ab8ec9a67d8ad6eaa5babdbf
Gaurvendra <44363966+Gaurvendra@users.noreply.github.com>
2021-04-07 16:34:26 +0530

    Update config.php

--- includes/protected/config.php
+++ includes/protected/config.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysql_connect("localhost", "root", "") or die("Could not connect to MySQL"); #pass=&b^JpCcic9d^SJ&
+$con = mysql_connect("localhost", "root", "3Nfx0w@1") or die("Could not connect to MySQL"); #pass=&b^JpCcic9d^SJ&
 mysql_select_db("ihack",$con) or die("Could not select db");
 
 ?>
